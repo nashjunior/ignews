6235517f0fb25e25925baab697692616428a6036
nashjunior <nashjuniorjj@gmail.com>
2021-12-08 18:24:36 -0300

    feat: :sparkles: initial layout config

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <Head>
+                <title>Document2</title>
+            </Head>
+            hello world
+        </div>
+    );
+};
+
+export default Home;
